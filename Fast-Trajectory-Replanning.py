7ee5ffa05397331b69575487c4fad2175b9579aa
amber-ku <72415577+amber-ku@users.noreply.github.com>
2021-10-09 12:46:23 -0400

    Update Fast-Trajectory-Replanning.py

--- Fast-Trajectory-Replanning.py
+++ Fast-Trajectory-Replanning.py
@@ -1,4 +1,5 @@
 
+from typing import List
 from matplotlib import animation
 import numpy as np
 from numpy.lib.type_check import asfarray
@@ -82,19 +83,16 @@ def print_node_list(list):
     for n in list:
         print_node(n)
 
-def get_path(start, goal):
+def get_path(start, goal)->List:
+    print('get path')
     path = []
     curr = goal
-
-
-    while curr is not None:
+    print_node(curr)
+    print_node(curr.parent)
+    print(len(path))
+    while curr is not None and curr.index != start.index:
         path.insert(0, curr.index)
         curr = curr.parent
-
-    print('Assume path:')
-    for p in path:
-        print('({x},{y})'.format(x=p[0], y=p[1]))
-
     return path
 
 def print_state(state, counter,comment = ""):
@@ -122,32 +120,47 @@ def move_to(start, goal, path, state, maze):
 
     path.pop()
     curr = start
+    # print('start',end="")
+    # print(curr)
+    # print_state(state, 1)
+
+
     for move in path:
-        row = curr[0] + move[0]
-        col = curr[1] + move[1]
+        row = move[0]
+        col = move[1]
+        
         if state[row][col] == -1:
             return (curr[0],curr[1])
         else:
             curr = (row, col)
             observe(curr, state, maze)
+
+    # print(curr)
     return curr
 
 
 
 def get_next(pos, grid, goal_idx, counter):
     next_list = []
+    print('curr pos {}'.format(pos))
+
     for move in neighbors:
         row = pos[0] + move[0]
         col = pos[1] + move[1]
+        print('({x},{y})'.format(x=row,y=col))
         if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[row])\
           or grid[row][col] == -1 :
+            print('out bound')
             continue
 
         if grid[row][col] == counter + 1:
-            continue;
+            print('visit')
+            continue
         next_list.append(n.node((row, col), goal_idx, gval=math.inf))
 
-
+    for p in next_list:
+        print('({x},{y})'.format(x=p.index[0],y=p.index[1]),end="")
+    
     return next_list
 
 
@@ -161,29 +174,38 @@ def compute_path(known_grid, start_node, goal_node, openlist, counter):
     # print(len(openlist))
     
     goal = goal_node
+    k = 0
+
 
     while len(openlist) > 0 and goal.gval > openlist[0].get_f():
+        k += 1
+        if k > 20:
+            break
+        print('\ncurr top    ',end=":")
         curr = heap.heappop(openlist)
+        print_node(curr)
+        print('curr goal     ',end=":")
+        print_node(goal)
 
-        if curr.index == goal_node.index:
-            goal = next_node
-            print('Goal node update',end="")
-            print_node(goal)
-            goal.parent = curr
+
             # print_state(known_grid,0)
             
         # print('min curr',end="")
         # print_node(curr)
         # print('open list size {}'.format(len(openlist)))
         actions = get_next(curr.index, known_grid, goal_node.index, counter)
+        # print_state(known_grid,0)
         # print('valid action {}'.format(len(actions)))
-        
 
         for next_node in actions:
             row = next_node.index[0]
             col = next_node.index[1]
-            print_node(next_node)
             
+            if next_node.index == goal_node.index:
+                goal = next_node
+                print('Goal node update',end="")
+                print_node(goal)
+                
             if known_grid[row][col] < counter:
                 next_node.gval = math.inf
                 known_grid[row][col] = counter
@@ -195,13 +217,13 @@ def compute_path(known_grid, start_node, goal_node, openlist, counter):
 
                 if next_node in openlist:
                     print('remove',end="")
-                    print_node(next_node)
+                    # print_node(next_node)
                     openlist.remove(next_node)
                 # print("push  ",end = "")
                 # print_node(next_node)
                 heap.heappush(openlist, next_node)
                 # print("top    ",end = "")
-                # print_node(openlist[0])
+                # print
 
     if len(openlist) == 0:
         return None
@@ -209,7 +231,12 @@ def compute_path(known_grid, start_node, goal_node, openlist, counter):
     # print_node(openlist[0])
     # goal.parent = curr
     # print_node(goal)
-    return get_path(start_node, curr)
+    path = get_path(start_node, curr)
+    
+    for p in path:
+            print('({x},{y})->'.format(x=p[0],y=p[1]),end="")
+    
+    return path
 
     # get_path()
         
@@ -254,31 +281,44 @@ print_state(state_grid, counter)
 goal_idx = (5, 4)
 goal = n.node((5, 4), goal_idx, gval=math.inf)
 curr = n.node(start, goal_idx, gval=0)
-# while curr.index != goal.index:
-#     print_state(state_grid, counter)
-#     counter += 1
-#     curr.gval = 0
-#     state_grid[curr.index[0]][curr.index[1]] = counter
-#     goal.gval = math.inf
-#     state_grid[goal_idx[0]][goal_idx[1]] = counter
-#     openlist = []
-#     # MUST COMPUTE THINGS HERE
-#     heap.heappush(openlist, curr)
-#     # compute_path(state_grid, curr, goal, openlist, counter)
-#     if len(openlist) == 0:
-#         print("I cannot reach the target")
-#         break
-#     path = get_path(curr, goal)
-#     curr.index = move_to(curr.index, goal.index, path, state_grid, maze)
+i = 0
+while i < 15 and curr.index != goal.index:
+    i +=  1
+    print_state(state_grid, counter)
+    counter += 1
+    curr.gval = 0
+    state_grid[curr.index[0]][curr.index[1]] = counter
+    goal.gval = math.inf
+    state_grid[goal_idx[0]][goal_idx[1]] = counter
+    openlist = []
+    # MUST COMPUTE THINGS HERE
+    heap.heappush(openlist, curr)
+    path = compute_path(state_grid, curr, goal, openlist, counter)
+    if path is None:
+        print("I cannot reach the target")
+        break
+    else:
+        # for p in path:
+        #     print('({x},{y})->'.format(x=p[0],y=p[1]),end="")
+        curr.index = move_to(curr.index, goal.index, path, state_grid, maze)
+        print('agent current position:({x},{y})'.format(x=curr.index[0], y=curr.index[1]))
+
+    
 
 
 
 openlist = []
 heap.heappush(openlist, curr)
 path = compute_path(maze, curr, goal, openlist, 1)
+
 if path is not None:
-    curr.index = move_to(curr.index, goal.index, path, state_grid, maze)
-print(curr.index)
+    for p in path:
+        print(p)
+    curr.index  = move_to(curr.index, goal.index, path, state_grid, maze)
+
+print_node(curr)
+
+
 # print_node(curr)
 # print_node(goal)
 
